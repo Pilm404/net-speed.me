c3186a55cedede610daaa8153ac2e06be5752db9
dt <dt@dt.dt>
2025-08-18 14:47:02 +0300

    The logic for obtaining values in the share.html and routes.py files has been changed.

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,17 @@ def about_me():
 
 @app.route('/share/<string:share_code>')
 def share(share_code):
-    return render_template('share.html', uuid=share_code)
+    field = db.session.scalar(
+        sa.select(SpeedShare).where(SpeedShare.code == share_code))
+    if field is None:
+        return render_template('error.html', error_code=404), 404
+
+    dp = min((field.download_speed or 0) / 100 * 100, 100)
+    up = min((field.upload_speed or 0) / 100 * 100, 100)
+    pp = min((field.ping or 0) / 100 * 100, 100)
+
+    return render_template('share.html', uuid=share_code, download=field.download_speed, upload=field.upload_speed,
+                            ping=field.ping, download_progress=dp, upload_progress=up, ping_progress=pp)
 
 @app.route('/share/delete/<string:share_code>')
 def share_delete(share_code):
